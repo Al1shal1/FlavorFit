4a1ad25530e0887ab920c5f2989b35078820373c
Alina <alishaliss09@gmail.com>
2026-02-05 18:53:16 +0300

    feat(auth): добавить логин пользователя
    - реализован вход в систему
    - добавлена проверка email
    - добавлена проверка пароля
    - реализована работа с refreshToken в httpOnly cookies
    - протестировано через GraphQL Playground

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthResolver } from './auth.resolver';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import { getJwtConfig } from 'src/config/jwt.config';
+import { UsersModule } from './../users/users.module'
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthResolver } from './auth.resolver'
+import { PrismaModule } from 'src/prisma/prisma.module'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
+import { getJwtConfig } from 'src/config/jwt.config'
+import { UsersService } from 'src/users/users.service'
 
 @Module({
-  imports: [PrismaModule, JwtModule.registerAsync({
-    imports: [PrismaModule],
-    inject: [ConfigService],
-    useFactory: getJwtConfig
-  })],
-  providers: [AuthService, AuthResolver]
+	imports: [
+		PrismaModule,
+		JwtModule.registerAsync({
+			imports: [PrismaModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		}),
+		UsersModule
+	],
+	providers: [AuthService, AuthResolver, UsersService]
 })
 export class AuthModule {}
--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,16 +1,31 @@
-import { AuthInput } from './auth.input';
-import { AuthResponse } from './auth.interface';
-import { AuthService } from './auth.service';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import type { IGqlContext } from 'src/app.interface'
+import { AuthInput } from './auth.input'
+import { AuthResponse } from './auth.interface'
+import { AuthService } from './auth.service'
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql'
 
 @Resolver()
 export class AuthResolver {
-    constructor(
-        private authService: AuthService
-    ){}
+	constructor(private authService: AuthService) {}
 
-    @Mutation(() => AuthResponse)
-    async register(@Args('data') input: AuthInput){
-        return this.authService.register(input)
-    }
+	@Mutation(() => AuthResponse)
+	async login(@Args('data') input: AuthInput, @Context() { res }: IGqlContext) {
+		const { refreshToken, ...response } = await this.authService.login(input)
+
+		this.authService.toggleRefreshTokenCookie(res, refreshToken)
+
+		return response
+	}
+
+	@Mutation(() => AuthResponse)
+	async register(
+		@Args('data') input: AuthInput,
+		@Context() { res }: IGqlContext
+	) {
+		const { refreshToken, ...response } = await this.authService.register(input)
+
+		this.authService.toggleRefreshTokenCookie(res, refreshToken)
+
+		return response
+	}
 }
